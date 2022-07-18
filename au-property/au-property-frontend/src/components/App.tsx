16a6feed7413046ce0fb168af8a8717a25edcb9a
Brandon Li <malasitan1@gmail.com>
2022-07-19 00:06:16 +1000

    Environment Setup New

--- au-property/au-property-frontend/src/components/App.tsx
+++ au-property/au-property-frontend/src/components/App.tsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-          <Button type="primary">Button</Button>
+            <Layout>
+                <Sider>Sider</Sider>
+                <Layout>
+                    <Header>Header</Header>
+                    <Content>Content</Content>
+                    <Footer>Footer</Footer>
+                </Layout>
+            </Layout>
         </div>
     );
   }
